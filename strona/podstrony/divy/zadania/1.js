e491aa86eb7830c5e0606526786e8b57a652e56a
Bozek202 <161302902+Bozek202@users.noreply.github.com>
2024-04-08 11:26:08 +0200

    Delete strona/podstrony/divy/zadania/1.js

--- strona/podstrony/divy/zadania/1.js
+++
@@ -1,9 +0,0 @@
-function onInputBlur(input) {
-        if(input.value == '')
-                input.value = input.defaultValue;
-}
-
-function onInputFocus(input) {
-        if(input.value == '' || input.value == input.defaultValue)
-                input.value = '';
-}
